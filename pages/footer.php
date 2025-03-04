6949af2fb72a1978e5510f3effc61ed8c9c3855f
lieutai997 <lieutai36@gmail.com>
2025-03-04 11:58:53 +0700

    thietlap trang giao dien

---
+++ pages/footer.php
@@ -0,0 +1,5 @@
+<div class="clear"></div>
+        <div class="footer">
+            <p class="footer_text">Copyright by LT develope 2025</p>
+        </div>
+    </div>
---
+++ pages/main.php
@@ -0,0 +1,27 @@
+<div id="main">
+    <?php include("sidebar/sidebar.php")
+    ?>
+            <div class="mainchinh">
+            <?php
+            if(isset($_GET['quanly'])){
+                $tam=$_GET['quanly'];
+            }else{
+                $tam='';
+            }
+            if($tam=='danhmucsanpham'){
+                include("main/danhmuc.php");
+            }elseif($tam=='giohang'){
+                include("main/giohang.php");
+            }elseif($tam=='tintuc'){
+                include("main/tintuc.php");
+            }elseif($tam=='lienhe'){
+                include("main/lienhe.php");
+            }elseif($tam=='sanpham'){
+                include("main/chitietsanpham.php");
+            }else {
+                include("main/index.php");
+            }
+
+            ?>
+            </div>
+        </div>
