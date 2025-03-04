a77d558408c71fa71b7a600e22b7dfe5bcd0d96c
lieutai997 <lieutai36@gmail.com>
2025-03-04 12:02:38 +0700

    trang hien thi

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    <title>WEB VẬT PHẨM PHẬT GIÁO</title>
+</head>
+
+<body>
+    
+
+    <div class="wrapper">
+        <?php
+        include("admincp/config/config.php");
+        include("pages/header.php");
+        include("pages/menu.php");
+        include("pages/main.php");
+        include("pages/footer.php");
+        ?>
+
+
+</body>
+
+</html>
