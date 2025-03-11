778db6bd68eed1764195e34f7a02ed99bb3b0515
lieutai997 <lieutai36@gmail.com>
2025-03-12 00:18:20 +0700

    update trang chinh

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+   
+    <link rel="stylesheet" type="text/css" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <title>WEB VẬT PHẨM PHẬT GIÁO</title>
 </head>
 
@@ -17,8 +19,8 @@
         <?php
         include("admincp/config/config.php");
         include("pages/header.php");
-         include("pages/menu.php");
-         include("pages/main.php");
+        include("pages/menu.php");
+        include("pages/main.php");
         include("pages/footer.php");
         ?>
 
