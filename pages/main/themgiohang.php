78018e756d047873cd63d61ad15ee188efab41ac
lieutai997 <lieutai36@gmail.com>
2025-03-12 00:17:40 +0700

    them cong xoa gio hang

--- pages/main/themgiohang.php
+++ pages/main/themgiohang.php
@@ -2,8 +2,74 @@
 session_start();
 include('../../admincp/config/config.php');
 //them soluong
+if(isset($_GET['cong'])){
+    $id=$_GET['cong'];
+    foreach($_SESSION['cart'] as $cart_item){
+        if($cart_item['id']!=$id){
+            $product[]=array ('tensanpham'=>$cart_item['tensanpham'],'id'=>$cart_item['id'],'soluong'=>$cart_item['soluong'],
+            'giasanpham'=>$cart_item['giasanpham'],'hinhanh'=>$cart_item['hinhanh'],'masanpham'=>$cart_item['masanpham']);
+            $_SESSION['cart'] = $product;
+        }else{
+            $tangsoluong = $cart_item['soluong'] + 1;
+            if($cart_item['soluong']<=10){
+                $product[]=array ('tensanpham'=>$cart_item['tensanpham'],'id'=>$cart_item['id'],'soluong'=>$tangsoluong,
+            'giasanpham'=>$cart_item['giasanpham'],'hinhanh'=>$cart_item['hinhanh'],'masanpham'=>$cart_item['masanpham']);
+
+            }else{
+                $product[]=array ('tensanpham'=>$cart_item['tensanpham'],'id'=>$cart_item['id'],'soluong'=>$cart_item['soluong'],
+            'giasanpham'=>$cart_item['giasanpham'],'hinhanh'=>$cart_item['hinhanh'],'masanpham'=>$cart_item['masanpham']);
+            }
+            $_SESSION['cart'] = $product;
+        }
+
+  
+
+    }
+    header('Location:../../index.php?quanly=giohang');
+}
+
 //tru soluong
+if(isset($_GET['tru'])){
+    $id=$_GET['tru'];
+    foreach($_SESSION['cart'] as $cart_item){
+        if($cart_item['id']!=$id){
+            $product[]=array ('tensanpham'=>$cart_item['tensanpham'],'id'=>$cart_item['id'],'soluong'=>$cart_item['soluong'],
+            'giasanpham'=>$cart_item['giasanpham'],'hinhanh'=>$cart_item['hinhanh'],'masanpham'=>$cart_item['masanpham']);
+            $_SESSION['cart'] = $product;
+        }else{
+            $tangsoluong = $cart_item['soluong'] - 1;
+            if($cart_item['soluong']>1){
+                $product[]=array ('tensanpham'=>$cart_item['tensanpham'],'id'=>$cart_item['id'],'soluong'=>$tangsoluong,
+            'giasanpham'=>$cart_item['giasanpham'],'hinhanh'=>$cart_item['hinhanh'],'masanpham'=>$cart_item['masanpham']);
+
+            }else{
+                $product[]=array ('tensanpham'=>$cart_item['tensanpham'],'id'=>$cart_item['id'],'soluong'=>$cart_item['soluong'],
+            'giasanpham'=>$cart_item['giasanpham'],'hinhanh'=>$cart_item['hinhanh'],'masanpham'=>$cart_item['masanpham']);
+            }
+            $_SESSION['cart'] = $product;
+        }
+
+  
+
+    }
+    header('Location:../../index.php?quanly=giohang');
+}
+
 //xoa sanpham
+if(isset($_SESSION['cart'])&&isset($_GET['xoa'])){
+    $id=$_GET['xoa'];
+    foreach($_SESSION['cart'] as $cart_item){
+        if($cart_item['id']!=$id){
+            $product[]=array ('tensanpham'=>$cart_item['tensanpham'],'id'=>$cart_item['id'],'soluong'=>$cart_item['soluong'],
+            'giasanpham'=>$cart_item['giasanpham'],'hinhanh'=>$cart_item['hinhanh'],'masanpham'=>$cart_item['masanpham']);
+          
+        }
+        $_SESSION['cart'] = $product;
+        header('Location:../../index.php?quanly=giohang');
+    }
+}
+
+
 //thêm sản phẩm vào giỏ hàng
 if(isset($_POST['themgiohang'])){
     
@@ -21,17 +87,20 @@ if(isset($_POST['themgiohang'])){
         if (isset($_SESSION['cart'])){
             $found=false;
             foreach($_SESSION['cart'] as $cart_item){
+                // nếu dữ liệu trùng cộng thêm 
                 if($cart_item['id']==$id){
                     $product[]=array ('tensanpham'=>$cart_item['tensanpham'],'id'=>$cart_item['id'],'soluong'=>$soluong+1,
                     'giasanpham'=>$cart_item['giasanpham'],'hinhanh'=>$cart_item['hinhanh'],'masanpham'=>$cart_item['masanpham']);
                     $found = true;
                 }else{
-                    $product[]=array ('tensanpham'=>$cart_item['tensanpham'],'id'=>$cart_item['id'],'soluong'=>$soluong,
+                    // nếu dữ liệu không trùng 
+                    $product[]=array ('tensanpham'=>$cart_item['tensanpham'],'id'=>$cart_item['id'],'soluong'=>$cart_item['soluong'],
                     'giasanpham'=>$cart_item['giasanpham'],'hinhanh'=>$cart_item['hinhanh'],'masanpham'=>$cart_item['masanpham']);
                 }
 
             }
             if($found == false){
+                // liên kết dữ liệu new_product vs product
                 $_SESSION['cart']=array_merge($product,$new_product);
             }else{
                 $_SESSION['cart']=$product;
@@ -40,6 +109,7 @@ if(isset($_POST['themgiohang'])){
             $_SESSION['cart']=$new_product;
         }
     }
-    header('Location:../../index.php?quanly=giohang');
+    print_r($_SESSION['cart']);
+     header('Location:../../index.php?quanly=giohang');
 }
 ?>
