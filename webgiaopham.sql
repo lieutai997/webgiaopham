bd2f36b0974fc93a4362ab52caf794ea84b0b81c
lieutai997 <lieutai36@gmail.com>
2025-03-22 13:33:05 +0700

    csdulieu

--- webgiaopham.sql
+++ webgiaopham.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th2 22, 2025 lúc 08:30 AM
--- Phiên bản máy phục vụ: 10.4.22-MariaDB
--- Phiên bản PHP: 7.4.27
+-- Thời gian đã tạo: Th3 22, 2025 lúc 07:31 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,41 @@ CREATE TABLE `tbl_admin` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `admin_status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`id_admin`, `username`, `password`, `admin_status`) VALUES
+(1, 'vodanh', 'e10adc3949ba59abbe56e057f20f883e', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_dangky`
+--
+
+CREATE TABLE `tbl_dangky` (
+  `id_dangky` int(11) NOT NULL,
+  `tenkhachhang` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `matkhau` varchar(100) NOT NULL,
+  `dienthoai` varchar(20) NOT NULL,
+  `diachi` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_dangky`
+--
+
+INSERT INTO `tbl_dangky` (`id_dangky`, `tenkhachhang`, `email`, `matkhau`, `dienthoai`, `diachi`) VALUES
+(1, 'Tài ', 'taib2005822@student.ctu.edu.vn', '123456', '', '0964959802'),
+(2, 'Tài ', 'taib2005822@student.ctu.edu.vn', '123456', 'cần thơ', '0964959802'),
+(3, 'Tài ', 'taib2005822@student.ctu.edu.vn', '123456', '', '0964959802'),
+(4, 'Tài ', 'taib2005822@student.ctu.edu.vn', '123456', '0964959802', 'cần thơ'),
+(5, 'Tài ', 'taib2005822@student.ctu.edu.vn', '123456', '0964959802', 'cần thơ'),
+(6, 'Tài ', 'taib2005822@student.ctu.edu.vn', '123456', '0964959802', 'cần thơ');
 
 -- --------------------------------------------------------
 
@@ -44,15 +78,17 @@ CREATE TABLE `tbl_danhmuc` (
   `id_danhmuc` int(11) NOT NULL,
   `tendanhmuc` varchar(100) NOT NULL,
   `thutu` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `tbl_danhmuc`
 --
 
 INSERT INTO `tbl_danhmuc` (`id_danhmuc`, `tendanhmuc`, `thutu`) VALUES
-(30, 'Ngọc Chuỗi', 0),
-(38, 'Đá Quý', 2);
+(1, 'Ngọc Chuỗi', 0),
+(2, 'Đá Quý', 0),
+(39, 'Hình Phật', 0),
+(40, 'Chỉ Tay', 4);
 
 -- --------------------------------------------------------
 
@@ -71,14 +107,19 @@ CREATE TABLE `tbl_sanpham` (
   `noidung` text NOT NULL,
   `tinhtrang` int(11) NOT NULL,
   `id_danhmuc` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `tbl_sanpham`
 --
 
 INSERT INTO `tbl_sanpham` (`id_sanpham`, `tensanpham`, `masanpham`, `giasanpham`, `soluong`, `hinhanh`, `tomtat`, `noidung`, `tinhtrang`, `id_danhmuc`) VALUES
-(33, 'hoa van', 'dssss', '100000000000', 2, '1739909311_06d02d36c30b7b55221a.jpg', '    aaaaaaaaa', '        ssssssssssssss    ', 1, 0);
+(44, 'Hoa van', '111', '1000', 1, '1740287065_5ffda12c62d1587597c822f6f10efcc9.jpg', ' reawefdawsQ', '  EFWDA ', 1, 1),
+(45, 'qqqqqqqqq', '2222', '12', 1, '1740295205_78fb4e090242d53f01eeaccccac94f64.jpg', '  sdcv', '    dsa  ', 1, 1),
+(46, 'aaaaaa', '1111', '21', 2, '1740292475_5ab9d2ea8328719ce150b8655b6e86bb.jpg', 'ávsd', 'dvvv', 1, 2),
+(51, 'Phật Thích Ca 1', '004', '10000', 1, '1740294140_2.jpg', ' ', '   ', 1, 39),
+(52, 'Phật Thích Ca 3', '004', '10000', 1, '1740294088_images.jpg', '', '', 1, 39),
+(54, 'Hoa Van', '0002', '10000', 1, '1740380493_2f178d6d8866110dfba2834336c4906f.jpg', '', '', 1, 2);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -89,6 +130,12 @@ INSERT INTO `tbl_sanpham` (`id_sanpham`, `tensanpham`, `masanpham`, `giasanpham`
 --
 ALTER TABLE `tbl_admin`
   ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Chỉ mục cho bảng `tbl_dangky`
+--
+ALTER TABLE `tbl_dangky`
+  ADD PRIMARY KEY (`id_dangky`);
 
 --
 -- Chỉ mục cho bảng `tbl_danhmuc`
@@ -110,19 +157,25 @@ ALTER TABLE `tbl_sanpham`
 -- AUTO_INCREMENT cho bảng `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_dangky`
+--
+ALTER TABLE `tbl_dangky`
+  MODIFY `id_dangky` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT cho bảng `tbl_danhmuc`
 --
 ALTER TABLE `tbl_danhmuc`
-  MODIFY `id_danhmuc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id_danhmuc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT cho bảng `tbl_sanpham`
 --
 ALTER TABLE `tbl_sanpham`
-  MODIFY `id_sanpham` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id_sanpham` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
