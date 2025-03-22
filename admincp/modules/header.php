e2fadf07c1bca964e3b10f9fdb6c3cb5f79dbb13
lieutai997 <lieutai36@gmail.com>
2025-03-22 13:23:10 +0700

    hien thi ten tai khoan

--- admincp/modules/header.php
+++ admincp/modules/header.php
@@ -1,1 +1,13 @@
-<p> header AD</p>
+<?php
+    if(isset($_GET['dangxuat'])&&$_GET['dangxuat']==1){
+        unset($_SESSION['dangnhap']);
+        header('Location:login.php');
+    }
+?>
+
+<p>   <li><a href="index.php?dangxuat=1">Đăng xuất : 
+        <?php
+            if(isset($_SESSION['dangnhap'])){
+                echo $_SESSION['dangnhap'];
+            }
+    ?></a></li></p>
