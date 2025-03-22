286fc22b8016f02e58171f76cd172a00d367d359
lieutai997 <lieutai36@gmail.com>
2025-03-22 13:20:53 +0700

    them  trang dang nhap admin

---
+++ admincp/login.php
@@ -0,0 +1,66 @@
+<?php
+    session_start();
+    include('config/config.php');
+    if (isset($_POST['dangnhap'])){
+    $taikhoan = $_POST['username'];
+    $matkhau = md5($_POST['password']);
+    $sql = "SELECT * FROM tbl_admin WHERE username='".$taikhoan."'AND password='".$matkhau."'LIMIT 1";
+    $row = mysqli_query($mysqli,$sql);
+    $count = mysqli_num_rows($row);
+    if($count>0){
+        $_SESSION['dangnhap'] = $taikhoan;
+        header("Location:index.php");
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - A Pen by Mohithpoojary</title>
+  <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.2.0/css/all.css'>
+<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css'><link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="container">
+	<div class="screen">
+		<div class="screen__content">
+			<form class="login" action="" method="POST">
+				<div class="login__field">
+					<i class="login__icon fas fa-user"></i>
+					<input type="text" class="login__input" placeholder="User name / Email" name="username">
+				</div>
+				<div class="login__field">
+					<i class="login__icon fas fa-lock"></i>
+					<input type="password" class="login__input" placeholder="Password" name="password">
+				</div>
+				<button class="button login__submit"type="submit" name ="dangnhap">
+                    
+					<span class="button__text">Log In Now</span>
+					<i class="button__icon fas fa-chevron-right"></i>
+				</button>				
+			</form>
+			<div class="social-login">
+				<h3>Giáo Phẩm Phật Giáo</h3>
+				<div class="social-icons">
+					<a href="#" class="social-login__icon fab fa-instagram"></a>
+					<a href="#" class="social-login__icon fab fa-facebook"></a>
+					<a href="#" class="social-login__icon fab fa-twitter"></a>
+				</div>
+			</div>
+		</div>
+		<div class="screen__background">
+			<span class="screen__background__shape screen__background__shape4"></span>
+			<span class="screen__background__shape screen__background__shape3"></span>		
+			<span class="screen__background__shape screen__background__shape2"></span>
+			<span class="screen__background__shape screen__background__shape1"></span>
+		</div>		
+	</div>
+</div>
+<!-- partial -->
+  
+</body>
+</html>
